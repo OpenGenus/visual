2c18520c8b3d06261f2522e6f701e3b82fc1772d
Erick Lumunge <lumungep12@gmail.com>
2021-12-22 05:36:21 +0300

    replaced var with let

--- code/assets/js/visual/PathFindingAlgorithms/bfs.js
+++ code/assets/js/visual/PathFindingAlgorithms/bfs.js
@@ -1,9 +1,9 @@
-import { rowSize, colSize, weightType, algorithmType } from "../Grid/index.js";
+import { rowSize, colSize, algorithmType } from "../Grid/index.js";
 import { setWall } from "../Grid/createWalls.js";
 
 const gridContainer = document.querySelector("#gridContainer");
 const speedSlider = document.querySelector(".speedSlider");
-var time = speedSlider.value;
+let time = speedSlider.value;
 
 const changeColor = (node, count, cost) => {
 	setTimeout(() => {
@@ -19,7 +19,7 @@ const changeColor = (node, count, cost) => {
 
 const checkUpdateNode = (row, col, curr, checker, visited, count) => {
 	if (row >= 0 && col >= 0 && row < rowSize && col < colSize) {
-		var node = document.querySelector(`div[row="${row}"][col="${col}"]`);
+		let node = document.querySelector(`div[row="${row}"][col="${col}"]`);
 		let wall = parseInt(node.getAttribute("wall"));
 		if (wall == 1) return;
 		let prow = parseInt(curr.getAttribute("row"));
@@ -63,19 +63,19 @@ export const bfs = (x1 = 0, y1 = 0, x2 = rowSize - 1, y2 = colSize - 1) => {
 	time = 40 + (time - 1) * -2;
 	gridContainer.removeEventListener("mousedown", setWall);
 	gridContainer.removeEventListener("mouseover", setWall);
-	var startNode = document.querySelector(`div[row='${x1}'][col='${y1}']`);
-	var endNode = document.querySelector(`div[row='${x2}'][col='${y2}']`);
+	let startNode = document.querySelector(`div[row='${x1}'][col='${y1}']`);
+	let endNode = document.querySelector(`div[row='${x2}'][col='${y2}']`);
 
-	//disable start and refresh btn
-	var startBtn = document.querySelector(".start");
-	startBtn.style.visibility = "hidden";
-	var clearPathBtn = document.querySelector(".clearPath");
-	clearPathBtn.style.visibility = "hidden";
+	//disable start and clear path buttons
+	let startBtn = document.querySelector(".start");
+	let clearPathBtn = document.querySelector(".clearPath");
+	startBtn.setAttribute("disabled", "true");
+	clearPathBtn.setAttribute("disabled", "true");
 
 	//start algorithm
-	var visited = [startNode];
-	var checker = [startNode];
-	var count = 1;
+	let visited = [startNode];
+	let checker = [startNode];
+	let count = 1;
 
 	while (checker.length != 0) {
 		checker.sort((a, b) => {
@@ -116,9 +116,9 @@ export const bfs = (x1 = 0, y1 = 0, x2 = rowSize - 1, y2 = colSize - 1) => {
 		startNode.setAttribute("class", "pathNode");
 		while (endNode.getAttribute("parent") != "null") {
 			endNode.setAttribute("class", "chosenPath");
-			var coor = endNode.getAttribute("parent").split("|");
-			var prow = parseInt(coor[0]);
-			var pcol = parseInt(coor[1]);
+			let coor = endNode.getAttribute("parent").split("|");
+			let prow = parseInt(coor[0]);
+			let pcol = parseInt(coor[1]);
 			endNode = document.querySelector(
 				`div[row="${prow}"][col="${pcol}"]`
 			);
@@ -128,6 +128,7 @@ export const bfs = (x1 = 0, y1 = 0, x2 = rowSize - 1, y2 = colSize - 1) => {
 	}, count * time + 100);
 
 	setTimeout(() => {
-		clearPathBtn.style.visibility = "visible";
+		startBtn.removeAttribute("disabled");
+		clearPathBtn.removeAttribute("disabled");
 	}, count * time + 100);
 };
