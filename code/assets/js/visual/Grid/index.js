3084f40b9facb148cb992ea9a68a498821b78611
lumunge <lumungep12@gmail.com>
2022-02-02 01:34:44 +0300

    find paths function

--- code/assets/js/visual/Grid/index.js
+++ code/assets/js/visual/Grid/index.js
@@ -1,10 +1,10 @@
 import {
-	createEmptyBoard,
-	createBoard,
-	createStartNode,
-	createEndNode,
-	refreshBoard,
-	refreshEmptyBoard,
+  createEmptyBoard,
+  createBoard,
+  createStartNode,
+  createEndNode,
+  refreshBoard,
+  refreshEmptyBoard,
 } from "./createGrid.js";
 import { setWall } from "./createWalls.js";
 import { setObstacles } from "./generateObstacles.js";
@@ -13,10 +13,11 @@ import { bfs } from "../PathFindingAlgorithms/bfs.js";
 import { bfsIslands } from "../Islands/bfsIslands.js";
 import { dfsIslands } from "../Islands/dfsIslands.js";
 import { maxIsland } from "../Islands/largeIsland.js";
+import { findPaths } from "../PathFindingAlgorithms/findPaths.js";
 import {
-	bellmanFord,
-	relaxations,
-	bellmanStepsLength,
+  bellmanFord,
+  relaxations,
+  bellmanStepsLength,
 } from "../PathFindingAlgorithms/bellmanFord.js";
 
 // get dom elements
@@ -26,6 +27,7 @@ export const resetBtn = document.querySelector(".reset");
 const weightBtn = document.querySelector(".weight");
 const algoBtn = document.querySelector(".algo");
 export const startBtn = document.querySelector(".start");
+const findNextPath = document.querySelector(".findNext");
 export const wallBtn = document.querySelector(".setWalls");
 export const speedSlider = document.querySelector(".speedSlider");
 const islandAlgoBtn = document.querySelector(".islandsAlgo");
@@ -45,7 +47,7 @@ export var mouseDown = false;
 export var weightType = weightBtn.options[weightBtn.selectedIndex].value;
 export var algorithm = algoBtn.options[algoBtn.selectedIndex].value;
 export var islandAlgo =
-	islandAlgoBtn.options[islandAlgoBtn.selectedIndex].value;
+  islandAlgoBtn.options[islandAlgoBtn.selectedIndex].value;
 
 //steps arrays
 export let bfsSteps = [];
@@ -53,42 +55,44 @@ export let dfsSteps = [];
 export let bellmanSteps = [];
 export let dijkstrasPath = [];
 export let bellmanFordPath = [];
+export let visitedPaths = [];
 
 //event listeners
 gridContainer.addEventListener("mousedown", () => {
-	mouseDown = true;
+  mouseDown = true;
 });
 gridContainer.addEventListener("mouseup", () => {
-	mouseDown = false;
+  mouseDown = false;
 });
 gridContainer.addEventListener("mouseover", () => {
-	setWall;
+  setWall;
 });
 wallBtn.addEventListener("click", setObstacles);
 
 islandAlgoBtn.addEventListener("change", () => {
-	window.location.reload();
+  window.location.reload();
 });
 
 //clear path after traversal
 export const clearPath = () => {
-	gridContainer.addEventListener("mousedown", setWall);
-	gridContainer.addEventListener("mouseup", setWall);
-	gridContainer.addEventListener("mouseover", setWall);
-	if (
-		algorithmType.classList.contains("bfs") ||
-		algorithmType.classList.contains("dfs") ||
-		algorithmType.classList.contains("numIslands") ||
-		algorithmType.classList.contains("maxIsland")
-	) {
-		refreshEmptyBoard();
-	} else if (
-		algorithmType.classList.contains("dijkstras") ||
-		algorithmType.classList.contains("bellman-ford")
-	) {
-		refreshBoard();
-	}
-	startBtn.style.visibility = "visible";
+  gridContainer.addEventListener("mousedown", setWall);
+  gridContainer.addEventListener("mouseup", setWall);
+  gridContainer.addEventListener("mouseover", setWall);
+  if (
+    algorithmType.classList.contains("bfs") ||
+    algorithmType.classList.contains("dfs") ||
+    algorithmType.classList.contains("numIslands") ||
+    algorithmType.classList.contains("maxIsland") ||
+    algorithmType.classList.contains("findPaths")
+  ) {
+    refreshEmptyBoard();
+  } else if (
+    algorithmType.classList.contains("dijkstras") ||
+    algorithmType.classList.contains("bellman-ford")
+  ) {
+    refreshBoard();
+  }
+  startBtn.style.visibility = "visible";
 };
 
 resetBtn.addEventListener("click", () => location.reload());
@@ -102,223 +106,247 @@ stepsContainer.append(stepsTitle);
 
 //log steps for algorithm
 export const notification = (row, col, erow, ecol, cost, prevCost) => {
-	var step = document.createElement("div");
-	step.classList.add("step");
-	var push = document.createElement("p");
-	var explore = document.createElement("p");
-	var costText = document.createElement("p");
-	if (algorithmType.classList.contains("bellman-ford")) {
-		if (bellmanSteps.length == 0) {
-			push.textContent = `Selected (${row}, ${col}) as path.`;
-		} else {
-			if (
-				bellmanSteps.length <=
-				bellmanStepsLength - bellmanStepsLength / relaxations
-			) {
-				push.textContent = `Relaxing (${row}, ${col}): current cost ${prevCost}, updated cost ${
-					cost || "inf"
-				}.`;
-				explore.textContent = `Processing (${erow}, ${ecol}).`;
-			} else {
-				push.textContent = `Pushed (${row}, ${col}) to dist[] array.`;
-				explore.textContent = `Exploring (${erow}, ${ecol}).`;
-			}
-		}
-	} else if (
-		algorithmType.classList.contains("dijkstras") ||
-		algorithmType.classList.contains("bfs")
-	) {
-		if (bfsSteps.length == 0) {
-			push.textContent = `Selected (${row}, ${col}) as path.`;
-		} else {
-			push.textContent = `Pushed (${row}, ${col}) to array.`;
-			explore.textContent = `Processing (${erow}, ${ecol}).`;
-			costText.textContent = `Cost from source is ${cost}`;
-		}
-	} else if (algorithmType.classList.contains("dfs")) {
-		if (row == erow && col == ecol) {
-			push.textContent = `Pushed (${erow}, ${ecol}) to stack.`;
-			explore.textContent = `Exploring (${erow}, ${ecol}).`;
-		} else {
-			push.textContent = `Popped (${row}, ${col}) from stack.`;
-		}
-	}
-	step.appendChild(push);
-	step.appendChild(explore);
-	step.appendChild(costText);
-	stepsContainer.append(step);
-	stepsContainer.scrollTop = stepsContainer.scrollHeight;
+  var step = document.createElement("div");
+  step.classList.add("step");
+  var push = document.createElement("p");
+  var explore = document.createElement("p");
+  var costText = document.createElement("p");
+  if (algorithmType.classList.contains("bellman-ford")) {
+    if (bellmanSteps.length == 0) {
+      push.textContent = `Selected (${row}, ${col}) as path.`;
+    } else {
+      if (
+        bellmanSteps.length <=
+        bellmanStepsLength - bellmanStepsLength / relaxations
+      ) {
+        push.textContent = `Relaxing (${row}, ${col}): current cost ${prevCost}, updated cost ${
+          cost || "inf"
+        }.`;
+        explore.textContent = `Processing (${erow}, ${ecol}).`;
+      } else {
+        push.textContent = `Pushed (${row}, ${col}) to dist[] array.`;
+        explore.textContent = `Exploring (${erow}, ${ecol}).`;
+      }
+    }
+  } else if (
+    algorithmType.classList.contains("dijkstras") ||
+    algorithmType.classList.contains("bfs") ||
+    algorithmType.classList.contains("findPaths")
+  ) {
+    if (bfsSteps.length == 0) {
+      push.textContent = `Selected (${row}, ${col}) as path.`;
+    } else {
+      push.textContent = `Pushed (${row}, ${col}) to array.`;
+      explore.textContent = `Processing (${erow}, ${ecol}).`;
+      costText.textContent = `Cost from source is ${cost}`;
+    }
+  } else if (algorithmType.classList.contains("dfs")) {
+    if (row == erow && col == ecol) {
+      push.textContent = `Pushed (${erow}, ${ecol}) to stack.`;
+      explore.textContent = `Exploring (${erow}, ${ecol}).`;
+    } else {
+      push.textContent = `Popped (${row}, ${col}) from stack.`;
+    }
+  }
+  step.appendChild(push);
+  step.appendChild(explore);
+  step.appendChild(costText);
+  stepsContainer.append(step);
+  stepsContainer.scrollTop = stepsContainer.scrollHeight;
 
-	step.addEventListener("click", () => {
-		let node = document.querySelector(`div[row='${row}'][col='${col}']`);
-		let node1 = document.querySelector(`div[row='${erow}'][col='${ecol}']`);
-		setTimeout(() => {
-			node.setAttribute("class", "pathColor");
-			node1.setAttribute("class", "pathColor");
-		}, 1000);
-		node1.setAttribute("class", "manualStep");
-		node.setAttribute("class", "chosenPath");
-	});
+  step.addEventListener("click", () => {
+    let node = document.querySelector(`div[row='${row}'][col='${col}']`);
+    let node1 = document.querySelector(`div[row='${erow}'][col='${ecol}']`);
+    setTimeout(() => {
+      node.setAttribute("class", "pathColor");
+      node1.setAttribute("class", "pathColor");
+    }, 1000);
+    node1.setAttribute("class", "manualStep");
+    node.setAttribute("class", "chosenPath");
+  });
 };
 
 // step by step visualization
 let isPath = true;
 export const stepper = (steps) => {
-	gridContainer.removeEventListener("mousedown", setWall);
-	gridContainer.removeEventListener("mouseover", setWall);
-	// wallBtn.setAttribute("disabled", true);
-	if (isPath) {
-		clearPath();
-		bellmanFordPath.splice(
-			0,
-			bellmanFordPath.length - bellmanFordPath.length / 5
-		);
-		startBtn.setAttribute("disabled", "true");
-		clearPathBtn.setAttribute("disabled", "true");
-		stepsContainer.classList.remove("notification");
-		stepsContainer.classList.add("show");
-		wallBtn.setAttribute("disabled", "true");
-		isPath = false;
-	}
-	if (steps.length == 0) {
-		if (
-			algorithmType.classList.contains("dijkstras") ||
-			algorithmType.classList.contains("bfs")
-		) {
-			if (dijkstrasPath.length == 0) {
-				alert("Steps completed!");
-			} else {
-				//draw path
-				var pcol = dijkstrasPath[0][0];
-				var prow = dijkstrasPath[0][1];
-				setTimeout(() => {
-					node.setAttribute("class", "pathColor");
-				}, 1000);
-				node.setAttribute("class", "chosenPath");
-				notification(pcol, prow, 0, 0);
-				dijkstrasPath.shift();
-			}
-		} else if (algorithmType.classList.contains("bellman-ford")) {
-			if (bellmanFordPath.length == 0) {
-				alert("Bellman ford steps completed!");
-			} else {
-				//draw path
-				var pcol = bellmanFordPath[0][0];
-				var prow = bellmanFordPath[0][1];
-				let node = document.querySelector(
-					`div[row='${pcol}'][col='${prow}']`
-				);
-				node.setAttribute("class", "chosenPath");
-				notification(pcol, prow, 0, 0);
-				bellmanFordPath.shift();
-			}
-		} else {
-			alert("Completed Steps");
-		}
-	} else {
-		var cr = steps[0][0];
-		var cc = steps[0][1];
-		var cost = steps[0][2];
-		var er = steps[0][3];
-		var ec = steps[0][4];
-		let node = document.querySelector(`div[row='${cr}'][col='${cc}']`);
-		setTimeout(() => {
-			node.setAttribute("class", "pathColor");
-		}, 1000);
-		node.setAttribute("class", "chosenPath");
-		//relaxation for bellman ford nodes
-		let prevCost = node.innerHTML;
-		node.innerHTML = cost || "inf";
-		notification(cr, cc, er, ec, cost, prevCost);
-		steps.shift();
-	}
+  gridContainer.removeEventListener("mousedown", setWall);
+  gridContainer.removeEventListener("mouseover", setWall);
+  // wallBtn.setAttribute("disabled", true);
+  if (isPath) {
+    clearPath();
+    bellmanFordPath.splice(
+      0,
+      bellmanFordPath.length - bellmanFordPath.length / 5
+    );
+    startBtn.setAttribute("disabled", "true");
+    clearPathBtn.setAttribute("disabled", "true");
+    stepsContainer.classList.remove("notification");
+    stepsContainer.classList.add("show");
+    wallBtn.setAttribute("disabled", "true");
+    isPath = false;
+  }
+  if (steps.length == 0) {
+    if (
+      algorithmType.classList.contains("dijkstras") ||
+      algorithmType.classList.contains("bfs")
+    ) {
+      if (dijkstrasPath.length == 0) {
+        alert("Steps completed!");
+      } else {
+        //draw path
+        var pcol = dijkstrasPath[0][0];
+        var prow = dijkstrasPath[0][1];
+        setTimeout(() => {
+          node.setAttribute("class", "pathColor");
+        }, 1000);
+        node.setAttribute("class", "chosenPath");
+        notification(pcol, prow, 0, 0);
+        dijkstrasPath.shift();
+      }
+    } else if (algorithmType.classList.contains("bellman-ford")) {
+      if (bellmanFordPath.length == 0) {
+        alert("Bellman ford steps completed!");
+      } else {
+        //draw path
+        var pcol = bellmanFordPath[0][0];
+        var prow = bellmanFordPath[0][1];
+        let node = document.querySelector(`div[row='${pcol}'][col='${prow}']`);
+        node.setAttribute("class", "chosenPath");
+        notification(pcol, prow, 0, 0);
+        bellmanFordPath.shift();
+      }
+    } else {
+      alert("Completed Steps");
+    }
+  } else {
+    var cr = steps[0][0];
+    var cc = steps[0][1];
+    var cost = steps[0][2];
+    var er = steps[0][3];
+    var ec = steps[0][4];
+    let node = document.querySelector(`div[row='${cr}'][col='${cc}']`);
+    setTimeout(() => {
+      node.setAttribute("class", "pathColor");
+    }, 1000);
+    node.setAttribute("class", "chosenPath");
+    //relaxation for bellman ford nodes
+    let prevCost = node.innerHTML;
+    node.innerHTML = cost || "inf";
+    notification(cr, cc, er, ec, cost, prevCost);
+    steps.shift();
+  }
+};
+
+const findNextPathFunc = (nodes) => {
+  let startNextRow = nodes.slice(-1)[0][0];
+  let startNextCol = nodes.slice(-1)[0][1];
+  let node = document.querySelector(
+    `div[row='${startNextRow}'][col='${startNextCol}']`
+  );
+  dfs(startRow, startCol, endRow, endCol);
+  nodes.pop();
+  clearPath();
+  if (parseInt(node.getAttribute("wall")) == 0) {
+    node.setAttribute("wall", 1);
+  }
 };
 
 //run normal visualization
 const startVisualization = () => {
-	if (algorithmType.classList.contains("bfs")) {
-		bfs(startRow, startCol, endRow, endCol);
-		manualStart.addEventListener("click", () => {
-			stepper(bfsSteps);
-		});
-	} else if (algorithmType.classList.contains("dfs")) {
-		dfs(startRow, startCol, endRow, endCol);
-		manualStart.addEventListener("click", () => {
-			stepper(dfsSteps);
-		});
-	} else if (algorithmType.classList.contains("dijkstras")) {
-		bfs(startRow, startCol, endRow, endCol);
-		manualStart.addEventListener("click", () => {
-			stepper(bfsSteps);
-		});
-	} else if (algorithmType.classList.contains("bellman-ford")) {
-		bellmanFord(startRow, startCol, endRow, endCol);
-		manualStart.addEventListener("click", () => {
-			stepper(bellmanSteps);
-		});
-	} else if (algorithmType.classList.contains("numIslands")) {
-		if (islandAlgo === "bfs") {
-			bfsIslands();
-		} else if (islandAlgo === "dfs") {
-			dfsIslands();
-		}
-	} else if (algorithmType.classList.contains("maxIsland")) {
-		maxIsland();
-	}
+  if (algorithmType.classList.contains("bfs")) {
+    bfs(startRow, startCol, endRow, endCol);
+    manualStart.addEventListener("click", () => {
+      stepper(bfsSteps);
+    });
+  } else if (algorithmType.classList.contains("dfs")) {
+    dfs(startRow, startCol, endRow, endCol);
+    manualStart.addEventListener("click", () => {
+      stepper(dfsSteps);
+    });
+  } else if (algorithmType.classList.contains("dijkstras")) {
+    bfs(startRow, startCol, endRow, endCol);
+    manualStart.addEventListener("click", () => {
+      stepper(bfsSteps);
+    });
+  } else if (algorithmType.classList.contains("bellman-ford")) {
+    bellmanFord(startRow, startCol, endRow, endCol);
+    manualStart.addEventListener("click", () => {
+      stepper(bellmanSteps);
+    });
+  } else if (algorithmType.classList.contains("numIslands")) {
+    if (islandAlgo === "bfs") {
+      bfsIslands();
+    } else if (islandAlgo === "dfs") {
+      dfsIslands();
+    }
+  } else if (algorithmType.classList.contains("maxIsland")) {
+    maxIsland();
+  } else if (algorithmType.classList.contains("findPaths")) {
+    findPaths(startRow, startCol, endRow, endCol);
+    findNextPath.addEventListener("click", () => {
+      findNextPathFunc(visitedPaths);
+    });
+    manualStart.addEventListener("click", () => {
+      stepper(dfsSteps);
+    });
+  }
 };
 startBtn.addEventListener("click", startVisualization);
+// findNextPath.addEventListener("click");
 
 //Initialize board
 window.onload = () => {
-	gridContainer.addEventListener("mousedown", setWall);
-	gridContainer.addEventListener("mouseup", setWall);
-	gridContainer.addEventListener("mouseover", setWall);
-	if (
-		algorithmType.classList.contains("bfs") ||
-		algorithmType.classList.contains("dfs") ||
-		algorithmType.classList.contains("numIslands") ||
-		algorithmType.classList.contains("maxIsland")
-	) {
-		createEmptyBoard();
-	} else if (
-		algorithmType.classList.contains("dijkstras") ||
-		algorithmType.classList.contains("bellman-ford")
-	) {
-		createBoard();
-	}
-	if (
-		algorithmType.classList.contains("bfs") ||
-		algorithmType.classList.contains("dfs") ||
-		algorithmType.classList.contains("dijkstras") ||
-		algorithmType.classList.contains("bellman-ford")
-	) {
-		createStartNode(startRow, startCol);
-		createEndNode(endRow, endCol);
-	} else if (
-		algorithmType.classList.contains("numIslands") ||
-		algorithmType.classList.contains("maxIslands")
-	) {
-		if (islandAlgo === "bfs") {
-			createStartNode(0, 0);
-			createEndNode(19, 39);
-			document
-				.querySelector(`div[row='${0}'][col='${0}']`)
-				.classList.add("islandsPathNode");
-			document
-				.querySelector(`div[row='${19}'][col='${39}']`)
-				.classList.add("islandsPathNode");
-		} else if (
-			islandAlgo === "dfs" ||
-			algorithmType.classList.contains("maxIslands")
-		) {
-			createStartNode(0, 0);
-			createEndNode(0, 1);
-			document
-				.querySelector(`div[row='${0}'][col='${0}']`)
-				.classList.add("islandsPathNode");
-			document
-				.querySelector(`div[row='${0}'][col='${1}']`)
-				.classList.add("islandsPathNode");
-		}
-	}
+  gridContainer.addEventListener("mousedown", setWall);
+  gridContainer.addEventListener("mouseup", setWall);
+  gridContainer.addEventListener("mouseover", setWall);
+  if (
+    algorithmType.classList.contains("bfs") ||
+    algorithmType.classList.contains("dfs") ||
+    algorithmType.classList.contains("numIslands") ||
+    algorithmType.classList.contains("maxIsland") ||
+    algorithmType.classList.contains("findPaths")
+  ) {
+    createEmptyBoard();
+  } else if (
+    algorithmType.classList.contains("dijkstras") ||
+    algorithmType.classList.contains("bellman-ford")
+  ) {
+    createBoard();
+  }
+  if (
+    algorithmType.classList.contains("bfs") ||
+    algorithmType.classList.contains("dfs") ||
+    algorithmType.classList.contains("dijkstras") ||
+    algorithmType.classList.contains("bellman-ford") ||
+    algorithmType.classList.contains("findPaths")
+  ) {
+    createStartNode(startRow, startCol);
+    createEndNode(endRow, endCol);
+  } else if (
+    algorithmType.classList.contains("numIslands") ||
+    algorithmType.classList.contains("maxIslands")
+  ) {
+    if (islandAlgo === "bfs") {
+      createStartNode(0, 0);
+      createEndNode(19, 39);
+      document
+        .querySelector(`div[row='${0}'][col='${0}']`)
+        .classList.add("islandsPathNode");
+      document
+        .querySelector(`div[row='${19}'][col='${39}']`)
+        .classList.add("islandsPathNode");
+    } else if (
+      islandAlgo === "dfs" ||
+      algorithmType.classList.contains("maxIslands")
+    ) {
+      createStartNode(0, 0);
+      createEndNode(0, 1);
+      document
+        .querySelector(`div[row='${0}'][col='${0}']`)
+        .classList.add("islandsPathNode");
+      document
+        .querySelector(`div[row='${0}'][col='${1}']`)
+        .classList.add("islandsPathNode");
+    }
+  }
 };
